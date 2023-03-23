cfde67b00264166ed79dc30e05cd9dc2aae2007d
Sadeeptha Bandara <sadeeptha.bandara@gmail.com>
2023-03-23 18:34:17 +0800

    Bug fixes for empty string, comparison at end of string

--- Week 1/z_algo.py
+++ Week 1/z_algo.py
@@ -1,5 +1,9 @@
 def z_algo(str):
     z_array = [None] * len(str)
+
+    if len(str) == 0:
+        return z_array
+
     z_array[0] = len(str)
     r, l = 0, 0
 
@@ -21,23 +25,38 @@ def z_algo(str):
             z_array[i] = compare_matches(str, 0, i)     
             r = i + z_array[i] - 1
             l = i
-
-        print(z_array)
     
     return z_array
 
 def compare_matches(str, start, end):
     count = 0
-    while str[start] == str[end]:
+
+    # Note that the order of the and operation is important to
+    # avoid an index error
+    while end < len(str) and str[start] == str[end]:
         count += 1
         start += 1
         end += 1
-
-        if end >= len(str):
-            break
     
     return count
 
 if __name__ == "__main__":
-    print(z_algo("ababac"))
+
+    # empty
+    print(z_algo(""))
+
+    # same char
     print(z_algo("aaaaaa"))
+
+    # different char
+    print(z_algo("abcdefg"))
+
+    # case 2a: z_(i-l) < rem
+    print(z_algo("abacabacc"))
+
+    # case 2b: z_(i-l) > rem
+    print(z_algo("abacababa"))
+
+    # case 2c: z_(i-l) = rem
+    print(z_algo("ababac"))
+
