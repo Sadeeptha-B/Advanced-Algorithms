c086dd13c6cad00680b7ae4a6373ed9be1a86129
Sadeeptha Bandara <sadeeptha.bandara@gmail.com>
2023-04-12 23:51:26 +0800

    Implemented str method for trie

--- Tree Based Data Structures/trie.py
+++ Tree Based Data Structures/trie.py
@@ -1,10 +1,18 @@
 class Trie:
+
     def __init__(self):
         self.root = Node()
+        self.count = 0
+        self.height = 0
 
-    def insert(self,key, data):
+    '''
+    Inserting a provided string and setting payload. If a duplicate is provided,
+    the payload is updated
+    '''
+    def insert(self,key, data=None):
         current = self.root
 
+        # Go through each char and create node if not already present
         for char in key:
             index = ord(char) - 97 + 1
 
@@ -13,15 +21,24 @@ class Trie:
             
             current = current.link[index]
 
-        te_ind = Node.TE_IND
+        te_ind = Node.terminal_index
 
         if current.link[te_ind] is None:
             current.link[te_ind] = Node()
 
         current = current.link[te_ind]
+
+        # Setting payload
         current.data = data
+        self.count += 1
+
+        if len(key) > self.height:
+            self.height = len(key)
 
 
+    '''
+    If key is found, returns the payload. If not found, raises a KeyError
+    '''
     def search(self, st):
         current = self.root
 
@@ -29,28 +46,46 @@ class Trie:
             index = ord(char) - 97 + 1
 
             if current.link[index] is None:
-                return False
+                raise KeyError('Key not found')
             
             current = current.link[index]
 
         if current.link[0] is not None:
             return current.link[0].data
         else:
-            return False
+            raise KeyError('Key not found')
     
 
-    '''Gives a sorted list of all strings stored in the trie'''
+    '''
+    Traverses over trie and returns a sorted list of all strings stored in the trie
+    '''
     def __str__(self):
         node = self.root
-
-        for i in range(len(node.link)):
-            pass
+        res = []
+        self.str_aux(node, res)
+        return res
             
+
+    def str_aux(self, node,  res, arr=[]):
+        for i in range(0, len(node.link)):
+            elem = node.link[i]
+
+            if elem is None:
+                continue
+
+            # Base
+            if i == 0:
+                res.append(''.join(arr))
+
+            else:
+            # Recursion
+                value = chr(i + 97 - 1)
+                self.str_aux(elem, res, arr.append(value))
+                
 
 
 class Node:
-
-    TE_IND = 0
+    terminal_index = 0
 
     def __init__(self, size=27):
         self.link = [None] * size
@@ -59,5 +94,9 @@ class Node:
 
 
 if __name__ == "__main__":
-    pass
+    trie = Trie()
+
+
+
+
     
