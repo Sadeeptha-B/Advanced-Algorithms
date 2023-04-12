4a2c44b30ecf92353653e09929f09227560a41f4
Sadeeptha Bandara <sadeeptha.bandara@gmail.com>
2023-04-13 00:20:38 +0800

    Suffix tree skeleton

---
+++ Tree Based Data Structures/suffix_tree.py
@@ -0,0 +1,19 @@
+'''
+Naive Implementation of suffix tree construction
+'''
+class SuffixTree:
+    def __init__(self, st):
+        self.root = self.buildTree()
+
+    def buildTree(self):
+        
+        
+        
+        pass
+
+class Node:
+    def __init__(self):
+        self.start = 0
+        self.end = 0
+
+    
