822f13e4fe1772de5a0067165d3ff27d49e7c7e5
Sadeeptha Bandara <sadeeptha.bandara@gmail.com>
2023-04-12 19:48:24 +0800

    Heap implementation

--- Tree Based Data Structures/heap.py
+++ Tree Based Data Structures/heap.py
@@ -1,59 +1,77 @@
 import math
 
+
 class MinHeap:
     def __init__(self, array):
         self.heap = self.buildHeap(array)
 
     def buildHeap(self, array):
-        # Write your code here.
-        pass
+        n = len(array)
 
-    def siftDown(self):
-        # Write your code here.
-        pass
+        if n <= 1:
+            return array
 
-    def siftUp(self):
-        ind = len(self) - 1
-        heap = self.heap
+        parent_ind = math.floor((n-2)/2)
+        for i in range(parent_ind, -1, -1):
+            self.siftDown(i, array)
 
-        while ind >= 2:
-            parent_ind =  math.floor((ind -1)/2)
-            left = parent_ind * 2 + 1
-            right = (parent_ind + 1) * 2 
+        return array
+    
 
-            if right >= len(self):
-                if heap[parent_ind] > heap[left]:
-                    heap[parent_ind], heap[left] = heap[left], heap[parent_ind]
-                    ind -= 1
-                    continue
+    def siftDown(self, ind, array):
+        left = 2*ind + 1
+        right = 2*(ind +1)
+        n = len(array)
 
-            min_child = heap[left]
+        if left >= n:
+            return 
 
-            if heap[left]> heap[right]:
-                min_child = heap[right]
+        if right >= n:
+            right = left
 
-            if heap[parent_ind] > heap[min_child]:
-                heap[parent_ind], heap[min_child] = heap[min_child], heap[parent_ind]
-
-            ind -= 1
+        # Get minimum child index
+        min_child = left if array[left] <= array[right] else right       
+       
+        # Swap if parent larger and continue
+        if array[ind] > array[min_child]:
+            array[ind], array[min_child] = array[min_child], array[ind]
+            self.siftDown(min_child, array)   
             
 
+    def siftUp(self, ind):
+        par_ind = math.floor((ind-1)/2)
+
+        # Loop while parent is larger than child
+        while ind > 0 and self.heap[par_ind] > self.heap[ind]:
+            # swap
+            self.heap[par_ind], self.heap[ind] = self.heap[ind], self.heap[par_ind]
+            ind = par_ind
+            par_ind = math.floor((par_ind-1)/2)
+        
+
     def peek(self):
-        # Write your code here.
         if len(self.heap) <= 0:
             return None
-
         return self.heap[0]
             
 
     def remove(self):
-        # Write your code here.
-        pass
+        if len(self.heap) <= 0:
+            return None
+
+        # swap root and end
+        self.heap[0], self.heap[-1] = self.heap[-1], self.heap[0]
+        elem = self.heap.pop()
+
+        self.siftDown(0, self.heap)
+        return elem
+        
 
     def insert(self, value):
         # Write your code here.
         self.heap.append(value)
-        self.siftUp()
+        n = len(self.heap)
+        self.siftUp(n-1)
 
-    def __len__(self):
-        return len(self.heap)
+        
+    
