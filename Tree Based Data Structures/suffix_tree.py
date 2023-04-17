c7cb09d9b0652f7877141d1f7b4246d48d99aa50
Sadeeptha Bandara <sadeeptha.bandara@gmail.com>
2023-04-17 23:35:29 +0800

    Added directories for upcoming content

--- Tree Based Data Structures/suffix_tree.py
+++ Tree Based Data Structures/suffix_tree.py
@@ -4,14 +4,13 @@ Naive Implementation of suffix tree construction
 class SuffixTree:
     def __init__(self, st):
         self.root = self.buildTree()
+        self.st = st
 
-    def buildTree(self):
-        
-        
-        
+    def buildTree(self, st):
+               
         pass
 
-class Node:
+class Edge:
     def __init__(self):
         self.start = 0
         self.end = 0
