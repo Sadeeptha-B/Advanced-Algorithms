b42971bef6d6d448b21a9bb834fcb0b19a9c6cdf
Sadeeptha Bandara <sadeeptha.bandara@gmail.com>
2023-04-27 11:50:36 +0800

    Added documentation, made get_bitarray public and made some modifications to support this, including resetting bitptr when file is closed

--- 30769140/q2/bwtunzip.py
+++ 30769140/q2/bwtunzip.py
@@ -1,5 +1,4 @@
 import sys
-import warnings
 
 OUTPUT_FILE = 'recovered.txt'
 BYTE_SIZE = 8 # 1 byte is 8 bits
@@ -25,6 +24,7 @@ class Decoder:
         self.bwt_length = self.decode_elias()
         self.unique_bwt = self.decode_elias()
 
+        print(self.bwt_length, self.unique_bwt)
 
         # Decode bwt characters and ascii encodings
         for _ in range(self.unique_bwt):
@@ -69,7 +69,10 @@ class Decoder:
        
             
 
-
+'''
+BinaryReader class to handle the logic of reading bytes and unpacking bits.
+Provides read_bit, read_bitarray, read_bits, read_bitstr methods.
+'''
 class BinaryReader: 
 
     def __init__(self, filename):        
@@ -81,17 +84,19 @@ class BinaryReader:
         self.__bitptr = 0
         self.__bitarray = None
 
-        
+
+    # Allow to change the filename if not file is open
     def set_file(self, filename):
         if self.__file is not None:
             raise IOError('Close currently opened file')
         
         if filename is None:
-            raise TypeError('Filename cannot be None')
+            raise ValueError('Filename cannot be None')
 
         self.__filename = filename
 
 
+    # Must be run before running file operations
     def open_file(self, filename=None):
         if self.__file is not None:
             raise IOError('Close currently opened file')
@@ -101,16 +106,30 @@ class BinaryReader:
 
         self.__file = open(self.__filename, 'rb')
 
+
+    # Must be called at the end of writing operation
     def close_file(self):
+        self.__bitptr = 0
         if self.__file is not None:
             self.__file.close()
 
         self.__file = None
 
 
-    def __read_bitarray(self):
+    '''
+    Handles the logic of reading a byte from a file and delegates to __parse_byte method
+    to construct a bitarray
+
+    - If called while buffer is not empty, it will not read anything
+    - bitarray will be set to an empty list if there are no further bytes to read
+    - To access the bit_array, use get_bitarray method
+    '''
+    def read_bitarray(self):
         if self.__file is None:
             raise IOError("File needs to be open first")
+        
+        if 0 < self.__bitptr < BYTE_SIZE:
+            return 
 
         file = self.__file
         byte = file.read(1)
@@ -124,28 +143,35 @@ class BinaryReader:
         self.__bitarray = self.__parse_byte((zeros, num)) 
         self.__bitptr = 0
 
-
-    def __parse_byte(self, byte_lst):
-        zeros, num = byte_lst
+    '''
+    Constructs and returns a bitarray when provided a tuple of the form
+    (no of pad zeros, number)
+    '''
+    def __parse_byte(self, byte_tup):
+        zeros, num = byte_tup
         bitarray = []
 
         for _ in range(zeros):
             bitarray.append(0)
 
-        bits = num.bit_length() - 1
+        shift_bit = num.bit_length() - 1
 
         for _ in range(num.bit_length()):
-            start = num >> bits
+            start = num >> shift_bit
             lsb = start % 2
             bitarray.append(lsb)
-            bits -= 1
+            shift_bit -= 1
 
         return bitarray
 
-
+    '''
+    Serves next bit according to bitptr. If the buffer end is reached,
+    it is refilled, until end of file.
+    Returns None if no further bits to read
+    '''
     def read_bit(self):
         if self.__bitarray is None:
-            self.__read_bitarray()
+            self.read_bitarray()
 
         if self.__bitptr >= BYTE_SIZE:
             raise RuntimeError('Pointer cannot exceed bitarray bounds')
@@ -157,11 +183,14 @@ class BinaryReader:
         self.__bitptr += 1
 
         if self.__bitptr >= BYTE_SIZE:
-            self.__read_bitarray()
+            self.read_bitarray()
 
         return bit
     
-
+    '''
+    Will read and return a list of specified
+    amount of bits
+    '''
     def read_bits(self, count):
         bits = []
         for _ in range(count):
@@ -171,24 +200,22 @@ class BinaryReader:
 
         return bits
     
+    
     def read_bitstr(self, count):
         bits = self.read_bits(count)
         return ''.join(bits)
     
 
     def get_bitarray(self):
+        if self.__bitarray is None:
+            return []
         return list(self.__bitarray)
+
 
     def __del__(self):
         if self.__file is not None:
             self.close_file()
 
-        
-
-        
-
-
-        
         
 
 
