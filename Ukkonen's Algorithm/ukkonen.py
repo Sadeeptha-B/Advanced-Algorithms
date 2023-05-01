b7e8b5e7bbabc88ede58b8dbe355e0e722b48bd4
Sadeeptha Bandara <sadeeptha.bandara@gmail.com>
2023-05-01 19:48:20 +0800

    Removed previous ukkonen function, created class and implemented global end

--- Ukkonen's Algorithm/ukkonen.py
+++ Ukkonen's Algorithm/ukkonen.py
@@ -1,86 +1,80 @@
 ALPHABET_SIZE = 91
 ASCII_START = 36
 
-def ukkonen_naive_loop(st):
-    st = st + "$"
-    n = len(st)
-    root = Node()
+class Ukkonen:
+    def __init__(self, st):
+        self.st = st
+        self.root = Node()
+        self.__globalend = End()
 
-    # Loop over phases
-    for i in range(n):
-        j = 0
-        # Loop over extensions
-        while j <= i:
-            chr = st[j]
-            ind = ord(chr) - ASCII_START
-            edge = root.edges[ind]
+        self.run()
 
-            if edge is None:
-                root.edges[ind] = Edge(j, i)
-                j+=1
-                continue
+    def run(self):
+        st = self.st + "$"
+        n = len(st)
+
+        # Initialize extension, global end
+        j = 0 
+        global_end = self.__globalend
+        global_end.set_value(0)
+        active_node = self.root
 
 
-            # Naive extension traversal
-            # Something similar to skip count is going on here
-            substr_len = i - j
-            while len(edge) < substr_len:
-
-                if edge.next is None:
-                    break
-                    
-                else:
-                    edge = edge.next
-                    substr_len = i - edge.end
-
-            start = edge.start
-
-            while start > end:
-                pass
+        # Loop over phases
+        for i in range(n):
             
-        
+            # Loop over extensions
+            while j <= i:
+                char = st[j]
+                ind = ord(char) - ASCII_START
+                edge = active_node.edges[ind]   # Use active node instead
 
+                if edge is None:
+                    self.root.edges[ind] = Edge(j, global_end)
+                    continue
 
+                # edge is not none
+                # implement skip count
+                # Once remainder of edge is reached, initiate rule 2 or rule 3
+                # if rule 2 create suffix link back to root
+                # next time active node will begin from there
+                # If rule 3, set active node active length and active edge
 
+            
+                j += 1
 
-
-
-            ind = edge.start
-
-            while ind < edge.end:
-                st[ind]
-
-
-            # Skip count optimization 
-            # substr_len = i - j
-
-        
-            # if substr_len > edge.end:
-                # Will have to jump to next edge (if any)
-                # Only at the last edge can I determine the case
-                # Much better to abstract global end so that I don't
-                # have to worry about it
-                # edge.end = i
-            # else:
-                # already at last edge
-                # case 2 or 3 
-                # traverse to the end and perform 
-                # if truly optimized, can just compare the necessary
-                # and perform extension
+            
+            # Increment j along with i if not rule 3 
+            # If rule 3 freeze
+            j += 1
                 
 
+            # Rule 1
+            global_end.increment()
 
-
-
-
-            j+= 1
             
-            # Traversal
-            pass
+
+    def generate_suffix_array(self):
+        arr = []
+        return arr
+
+      
+'''
+Class to keep track of global end since Python does not pass values by reference
+'''
+class End:
+    def __init_(self):
+        self.end = None
+
+    def set_value(self, value):
+        self.end = value
+
+    def increment(self, value=1):
+        self.end += value
 
 
-    return root
-            
+# Suffix tree components
+# ===============================================================================================
 
 class Node:
     def __init__(self):
@@ -92,14 +86,12 @@ class Edge:
         self.end= end
         self.next = None
 
-    def set_end(self, end):
-        self.end = end
-
-
     def __len__(self):
-        return self.end - self.start
+        return self.end - self.start + 1
 
 
 if __name__ == "__main__":
-    ukkonen_naive_loop("apple")
+    ukkonen = Ukkonen("abcabxabcyab")
+    suffix_array = ukkonen.generate_suffix_array()
+    print(suffix_array)
 
