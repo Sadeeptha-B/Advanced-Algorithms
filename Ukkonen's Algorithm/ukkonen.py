faf215db83f42d7ef469597f88271c600f1f46ed
Sadeeptha Bandara <sadeeptha.bandara@gmail.com>
2023-05-05 00:33:16 +0800

    Implemented alternate ukkonen

--- Ukkonen's Algorithm/ukkonen.py
+++ Ukkonen's Algorithm/ukkonen.py
@@ -21,7 +21,88 @@ class Ukkonen:
 
         # Link root to itself
         self.root.link = self.root
+        self.root.id = 0
         self.run()
+
+
+
+    def run1(self):
+        st = self.st + "$"
+        n = len(st)
+
+        j = 0 # Initial extension
+        active_node = self.root
+        active_edge = None
+        curr_ind = j
+        active_ptr = 0
+
+        for i in range(n):
+            self.__global_end.set_value(i)
+
+            edge = active_node.get_edge(st[curr_ind])
+
+            if edge is None:
+                active_node.set_edge(st[curr_ind], Edge(curr_ind, self.__global_end, j))
+                j += 1
+                curr_ind += 1
+                continue
+            
+            active_edge = edge
+            remaining = i - curr_ind + 1
+
+            # Skip count
+            while remaining > len(active_edge):
+                active_node = active_edge.next
+                
+                curr_ind += len(active_edge)
+                remaining -= len(active_edge)
+
+                active_edge = active_node.get_edge(st[curr_ind])
+
+                if active_edge is None:
+                    active_edge = Edge(curr_ind, self.__global_end, j)
+                    active_node.set_edge(st[curr_ind], active_edge)
+
+
+            # Rule 2 alternate
+            if curr_ind == i:
+                # Rule 2 alternate propagatino
+                continue
+
+            comp_ind = active_edge.start + remaining - 1 + active_ptr
+            
+            # Rule 3
+            if st[comp_ind] == st[i]:
+                active_ptr += 1
+                continue
+
+            # Rule 2: general       
+            self.create_new_node(active_edge, st, i, comp_ind, j)
+
+            # Propagate rule 2
+            while j <= i:
+
+                j += 1
+                curr_ind = j 
+
+
+
+
+
+            
+
+    def create_new_node(self, active_edge, st, i, comp_ind, j):
+        node = Node()
+        prev_path = Edge(comp_ind, active_edge.end, active_edge.suffix_id)
+        node.set_edge(st[i], Edge(i, self.__global_end, j))
+        node.set_edge(st[comp_ind], prev_path)  
+        prev_path.next = active_edge.next
+
+        active_edge.end = End(comp_ind - 1)
+        active_edge.next = node
+        active_edge.suffix_id = None
+        
+
 
 
     def run(self):
@@ -32,15 +113,20 @@ class Ukkonen:
         j = 0 
         active_node = self.root
         suffix_len = 0
+        node_count  = 1
 
         # Loop over phases
         for i in range(n):
             # Trick 1: Global end 
             self.__global_end.set_value(i)
-            while j < i:
                 
+            while j < i:
+               
                 # Trick 3: Skip count traversal
                 char_ind = i - suffix_len + 1
+                if j == 5 and i == 8:
+                    print(char_ind, suffix_len, len(active_edge), (active_edge.start, active_edge.end.value))
+                
 
                 while suffix_len > len(active_edge):
                     # Next node: this MUST be present
@@ -62,16 +148,23 @@ class Ukkonen:
                         active_edge = Edge(char_ind, self.__global_end, j)
                         active_node.edges[ord(suffix_char) - ASCII_START] = active_edge
 
+
+                    skip_count = True
                     # Rule 2 alternate: Suffix link
-                #     if active_edge is None:
-                #         while j < i:
-                #             active_edge = Edge(char_ind, self.__global_end)
-                #             active_node.edges[ord(suffix_char) - ASCII_START] = active_edge
-                #             active_node = active_node.link
-                #             j += 1
+                    # if active_edge is None:
+                    #     active_edge = Edge(char_ind, self.__global_end, j)
+                    #     while j < i:
+                    #         print(f"{j}, {i} rule 2 alt")
+                    #         active_node.edges[ord(suffix_char) - ASCII_START] = active_edge
+                    #         active_node = active_node.link
+                    #         # print(node.id)
+                    #         print(node.id)
+                    #         print("=======")
+                            
+                    #         j += 1
                 
                 if active_edge.start == i:
-                    # print(f"{j}, {i} rule 2 alt")
+                    print(f"{j}, {i} rule 2 alt")
                     active_node = active_node.link
                     active_ptr = 1
                     j += 1
@@ -80,29 +173,32 @@ class Ukkonen:
                     continue
                     
                           
-                # if j == i:
-                #     break
+                if j == i:
+                    break
       
 
                 # After skip count traversal: case 2 or case 3 must occur    
                 # Comparing the suffix extension char and the edge character
-                comp_ind = active_edge.start + active_ptr
+                comp_ind = active_edge.start + active_ptr 
                 edge_char = st[comp_ind]       
                 extension = st[i]
 
                 # Rule 3
                 if extension == edge_char:
-                    # print(f"{j}, {i} rule 3")
+                    print(f"{j}, {i} rule 3")
 
                     active_ptr += 1
                     suffix_len += 1
+                  
                     break
 
 
                 # Rule 2: General
-                # print(f"{j}, {i} rule 2 gen")
+                print(f"{j}, {i} rule 2 gen")
 
                 node = Node()    
+                node.id = node_count 
+                node_count += 1
                 node.edges[ord(extension) - ASCII_START] = Edge(i, self.__global_end, j)
                 node.edges[ord(edge_char) - ASCII_START] = Edge(comp_ind, active_edge.end, active_edge.suffix_id)
                 node.link = self.root
@@ -113,27 +209,31 @@ class Ukkonen:
                 active_edge.next = node
                 active_edge.suffix_id = None
 
+
                 # Preparing for next extension
                 active_node = active_node.link
                 active_ptr = 1
                 suffix_len -= 1
-
                 j += 1
                 active_edge = active_node.edges[ord(st[j]) - ASCII_START] 
+
+                if active_node == self.root:
+                    suffix_len = i - j + 1
 
 
             if j == i:
                 ind = ord(st[j]) - ASCII_START
-                edge = active_node.edges[ind]
+                edge = self.root.edges[ind]
 
                 # Rule 2 alternate
                 if edge is None:
                     # Trick 2: start, end representation
-                    # print(f"{j}, {i} rule 2 alt")
-                    active_node.edges[ind] = Edge(j, self.__global_end, j)
+                    print(f"{j}, {i} rule 2 alt:root")
+   
+                    self.root.edges[ind] = Edge(j, self.__global_end, j)
                     j += 1
                 else:
-                    # print(f"{j}, {i} rule 3")
+                    print(f"{j}, {i} rule 3")
                     active_edge = edge
                     active_ptr = 1
                     suffix_len = 1 + active_ptr
@@ -153,7 +253,7 @@ class Ukkonen:
                 continue
 
             if edge.next is None:
-                print(edge.start, edge.end.value, edge.suffix_id)
+                # print(edge.start, edge.end.value, edge.suffix_id)
                 arr.append(edge.suffix_id)
             else:
                 self.inorder_aux(edge.next, arr)
@@ -181,6 +281,15 @@ class Node:
     def __init__(self, link=None):
         self.edges = [None]*ALPHABET_SIZE
         self.link = None
+        self.id = None
+
+    def set_edge(self, char, edge):
+        ind = ord(char) - ASCII_START
+        self.edges[ind] = edge
+
+    def get_edge(self, char):
+        ind = ord(char) - ASCII_START
+        return self.edges[ind]
 
 
 class Edge:
