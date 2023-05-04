30a240f9a026964703060086206eff2a060077a9
Sadeeptha Bandara <sadeeptha.bandara@gmail.com>
2023-05-04 16:33:15 +0800

    Implemented inorder traversal and started debugging

--- Ukkonen's Algorithm/ukkonen.py
+++ Ukkonen's Algorithm/ukkonen.py
@@ -37,7 +37,6 @@ class Ukkonen:
         for i in range(n):
             # Trick 1: Global end 
             self.__global_end.set_value(i)
-
             while j < i:
                     
                 # Trick 3: Skip count traversal
@@ -60,7 +59,7 @@ class Ukkonen:
 
                     # Rule 2 alt: rooted
                     if active_edge is None:
-                        active_edge = Edge(char_ind, self.__global_end)
+                        active_edge = Edge(char_ind, self.__global_end, j)
                         active_node.edges[ord(suffix_char) - ASCII_START] = active_edge
 
                     # Rule 2 alternate: Suffix link
@@ -99,14 +98,15 @@ class Ukkonen:
 
                 # Rule 2: General
                 node = Node()    
-                node.edges[ord(extension) - ASCII_START] = Edge(i, self.__global_end)
-                node.edges[ord(edge_char) - ASCII_START] = Edge(comp_ind, active_edge.end)
+                node.edges[ord(extension) - ASCII_START] = Edge(i, self.__global_end, j)
+                node.edges[ord(edge_char) - ASCII_START] = Edge(comp_ind, active_edge.end, active_edge.suffix_id)
                 node.link = self.root
 
                 # Wiring up active_edge
                 active_edge.end = End(comp_ind - 1)
                 node.edges[ord(edge_char) - ASCII_START].next = active_edge.next
                 active_edge.next = node
+                active_edge.suffix_id = None
 
                 # Preparing for next extension
                 active_node = active_node.link
@@ -124,7 +124,7 @@ class Ukkonen:
                 # Rule 2 alternate
                 if edge is None:
                     # Trick 2: start, end representation
-                    active_node.edges[ind] = Edge(j, self.__global_end)
+                    active_node.edges[ind] = Edge(j, self.__global_end, j)
                     j += 1
                 else:
                     active_edge = edge
@@ -134,8 +134,18 @@ class Ukkonen:
 
     def generate_suffix_array(self):
         arr = []
+        self.inorder_aux(self.root, arr)
         return arr
+    
+    def inorder_aux(self, node, arr):
+        for edge in node.edges:
+            if edge is None:
+                continue
 
+            if edge.next is None:
+                arr.append(edge.suffix_id)
+            else:
+                self.inorder_aux(edge.next, arr)
 
 
 
@@ -163,17 +173,20 @@ class Node:
 
 
 class Edge:
-    def __init__(self, start, end):
+    def __init__(self, start, end, suffix_id = None):
         self.start = start
         self.end= end
         self.next = None
+        self.suffix_id = suffix_id
 
     def __len__(self):
         return self.end.value - self.start + 1
 
 
 if __name__ == "__main__":
-    ukkonen = Ukkonen("abcabxabcyab")
+    print(len("mississippi$"))
+    print("=====")
+    ukkonen = Ukkonen("mississippi")
     suffix_array = ukkonen.generate_suffix_array()
     print(suffix_array)
 
