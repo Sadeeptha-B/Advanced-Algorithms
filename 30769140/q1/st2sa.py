22270b83e6d596aa9641e0b1a26e49288ea1a1d0
Sadeeptha Bandara <sadeeptha.bandara@gmail.com>
2023-05-06 22:08:14 +0800

    Added documentation and small change to file writing

--- 30769140/q1/st2sa.py
+++ 30769140/q1/st2sa.py
@@ -2,13 +2,17 @@
 Author: Sadeeptha Bandara
 Student ID: 30769140
 '''
+
 import sys
 
 OUTPUT_FILE = "output_sa.txt"
-ALPHABET_SIZE = 91
-ASCII_START = 36
+ALPHABET_SIZE = 91  # 126 - 37 + 2
+ASCII_START = 36    #Starting ASCII char
 
 
+'''
+Class to modularize ukkonen components
+'''
 class Ukkonen:
     def __init__(self, st):
         self.st = st
@@ -19,30 +23,47 @@ class Ukkonen:
         self.run()
 
 
+    '''
+    Builds suffix tree in phases. In each phase i, constructs the implicit 
+    suffix tree for [0..i] (0 based indexing). Maintains the following variables
+
+    i - The current phase
+    j - The starting index of the suffix within a phase that is being extended
+    active_node - The current node during tree traversal
+    active_edge - The current edge during traversal (this is connected to the active_node)
+    curr_ind - Index within [j..i] that corresponds to the tree position of active_edge.start
+                (Useful when skip counting)
+    remaining - The remaining length of the suffix after traversal: len([curr_ind.. i])
+    '''
     def run(self):
         st = self.st + "$"
         n = len(st)
 
-        # Initial phase and initial extension
+        # Initialisation
         i, j = 0, 0 
         active_node = self.root
         active_edge = None
         curr_ind = j
         previous= None
 
+        # Loop over phases
         while i < n:
+
+            # Maintain global end
             self.__global_end.set_value(i)
             edge = active_node.get_edge(st[curr_ind])
 
+            # Handle all rule 2 alternate cases
             if edge is None:
                 active_node.set_edge(st[curr_ind], Edge(curr_ind, self.__global_end, j))
+                
+                # Suffix link manipulation
                 if previous is not None:
-
                     previous.link = active_node
                     previous = None
-                
                 active_node = active_node.link
 
+                # Move to next phase
                 if i == j:
                     previous = None
                     i += 1
@@ -64,6 +85,7 @@ class Ukkonen:
 
                 active_edge = active_node.get_edge(st[curr_ind])
 
+                # If rule 2 alternate is encountered during skip count
                 if active_edge is None:
                     node_found = True
                     break
@@ -72,30 +94,42 @@ class Ukkonen:
             if node_found:
                 continue
 
-
+            # Index on active edge to compare with i 
             comp_ind = active_edge.start + remaining - 1 
 
-            # Rule 3
+            # Handle rule 3: showstopper
+            # Moves to the next phase while freezing j till a rule 2 occurs
             if st[comp_ind] == st[i]:
+                
+                # Suffix link
                 if previous is not None:
                     previous.link = active_node
                 previous = None
+
                 i += 1
                 continue
                 
+
+            # Handle all rule 2 general cases
             node = self.create_new_node(active_edge, st, i, comp_ind, j)
 
+            # Suffix link manipulation
             if previous is not None:
                 previous.link = node
-
             previous = node
+
+            # Reset curr_ind
             if active_node is self.root:
                 curr_ind += 1
 
+            # Suffix link traversal
             active_node = active_node.link            
             j += 1
 
-         
+
+    '''
+    Called when a new internal node is being created. 
+    '''
     def create_new_node(self, active_edge, st, i, comp_ind, j):
         node = Node()
         prev_path = Edge(comp_ind, active_edge.end, active_edge.suffix_id)
@@ -110,7 +144,9 @@ class Ukkonen:
         
         return node
 
-
+    '''
+    Generate a suffix array from a constructed suffix tree
+    '''
     def generate_suffix_array(self):
         arr = []
         self.inorder_aux(self.root, arr)
@@ -128,9 +164,14 @@ class Ukkonen:
                 self.inorder_aux(edge.next, arr)
 
 
+
+
 # Suffix Tree components
 # ===================================================================================================
 
+'''
+Each node maintains a list of edges connected to it
+'''
 class Node:
     def __init__(self, link=None):
         self.edges = [None]*ALPHABET_SIZE
@@ -145,7 +186,10 @@ class Node:
         return self.edges[ind]
 
 
-
+'''
+Edges stores the start and end indices, also the next node connected to it, 
+if any. If the edge is a leaf, it will contain a non null suffix_id
+'''
 class Edge:
     def __init__(self, start, end, suffix_id = None):
         self.start = start
@@ -185,9 +229,13 @@ def open_file(filename):
 Takes in a zero-indexed suffix array and writes to output file in 1-indexed form
 '''
 def write_output(suffix_array):
+    n = len(suffix_array)
+
     with open(OUTPUT_FILE, 'w') as file:
-        for ind in suffix_array:
-            file.write(f"{ind+1}\n")
+        for i in range(n-1):
+            file.write(f"{suffix_array[i]+1}\n")
+        file.write(f"{suffix_array[-1]}")
+
             
 
 if __name__ == "__main__":
