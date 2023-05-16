0e51bd01c19473176047acf4da397c7366ae54d9
Sadeeptha Bandara <sadeeptha.bandara@gmail.com>
2023-05-16 23:20:47 +0800

    Modified file reading to perform the preprocessing required for tableau simplex, for efficiency

--- 30769140/q2/mysimplex.py
+++ 30769140/q2/mysimplex.py
@@ -19,52 +19,92 @@ def tableau_simplex(text):
     return ['5','9'], str(23)
 
 
-def read_file(filename):
+def preprocess_input(objective, constraints):
+    
+
+    pass
+
+# I/O operations
+# ================================================================================
+
+'''
+Reads specified file and preprocesses content to a format suitable for tableau simplex
+
+Returns --------------------------------------------------------------------------
+objective: A list with coefficients of the objective function in the provided order,
+padded to be the size of decision var + constraints to consider slack variables
+
+constraints_matrix: A list containing constraint lists. The size of a nested list
+is (no of decision var + no of constraints) to account for slack variables. The coefficients 
+of the slack variables have been added.
+
+rhs_values: A list containing the rhs values in float format
+
+All returned numbers are floats.
+'''
+def read_preprocess(filename):
     with open(filename, 'r') as file:
-        # Decision variables
+        # No of Decision variables
         file.readline()
         decisions = int(file.readline().strip())
 
-        # Constraints
+        # No of Constraints
         file.readline()
         constraints = int(file.readline().strip())
 
         # Objective
         file.readline()
-        objective = str_to_lst(file.readline(), decisions)
+        objective = get_padded_lst(file.readline(), decisions + constraints)
 
         # Constraints LHS
         file.readline()
-        
-        constraints_matrix = [[None, None] for _ in range(constraints)]
-        LHS_IND = 0
-        RHS_IND = 1
 
-        for i in range(constraints):
-            constraints_matrix[i][LHS_IND] = str_to_lst(file.readline(), decisions)
+        constraints_matrix = []
+        ptr = decisions
+        
+        for _ in range(constraints):
+            elem = get_padded_lst(file.readline(), decisions + constraints)
+            elem[ptr] = 1.0
+            constraints_matrix.append(elem)
+            ptr += 1
+
+        print(constraints_matrix)
 
         # Constraints RHS
         file.readline()
+        rhs_values = []
 
-        for i in range(constraints):
-            constraints_matrix[i][RHS_IND] = int(file.readline().strip())
+        for _ in range(constraints):
+            rhs_values.append(float(file.readline().strip()))
+
+        print(rhs_values)
+
+    return constraints, objective, constraints_matrix
 
 
-    return decisions, constraints, objective, constraints_matrix
 
-
-def str_to_lst(st, decisions):
+'''
+Given a numerical string, will return a list of the specified size with the 
+numbers converted to floats. If the specified size is larger than the length 
+of the string will pad the specified character
+'''
+def get_padded_lst(st, size, pad=0.0):
     if st[0] == "#":
         raise ValueError()
+    
+    if type(pad) is not float:
+        raise ValueError('Padding must be float')
 
     res =  st.strip().split(',')
 
-    if len(res) != decisions:
-        raise ValueError('No of coefficients should be same as number of decision variables')
+    for i, elem in enumerate(res):
+        res[i] = float(elem)
 
-    return [int(v) for v in res]
+    for _ in range(size - len(res)):
+        res.append(pad)
 
-      
+    return res
+  
         
 def write_output(decisions, optimal):
     headings = ["# optimalDecisions\n", "\n# optimalObjective\n"]
@@ -80,9 +120,8 @@ def write_output(decisions, optimal):
 if __name__ == "__main__":
     _, filename = sys.argv
 
-    # Read input format 
-    # TODO: change output to be of suitable form for the simplex algorithm
-    text = read_file(filename)
+    # Read input 
+    text = read_preprocess(filename)
 
     # Implement tableau simplex
     # decisions,optimal = tableau_simplex(text)
