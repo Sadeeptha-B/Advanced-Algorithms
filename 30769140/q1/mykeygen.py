a339928a573a2c6d6f158c2fdd603cf00ff38cb7
Sadeeptha Bandara <sadeeptha.bandara@gmail.com>
2023-05-21 00:07:00 +0800

    Added I/O operations to write to a file

--- 30769140/q1/mykeygen.py
+++ 30769140/q1/mykeygen.py
@@ -23,14 +23,31 @@ SECRET_FILE = "secretprimes.txt"
 # write primes and public key to two files
 
 
+# I/O operations
+# ========================================================================
+
+def write_to_file(filename, headings, outputs):
+    with open(filename, 'w') as file:
+        for ind, heading in enumerate(headings):
+            file.write(heading)
+            file.write(str(outputs[ind]))
+
 
 if __name__ == "__main__":
     _, d = sys.argv
 
     # Validate d to be int
+    if not d.isdigit():
+        raise ValueError('d must be an integer')
 
+    # generate_primes()
+    # compute_n()
+    # compute_d()
 
-    pass
+    keyfile_headings = ["# modulus (n)\n", "\n# exponent (e)\n"]
+    secretfile_headings = ["# p\n", "\n# q\n"]
 
+    write_to_file(KEY_FILE, keyfile_headings, [1073602561, 3187811])
+    write_to_file(SECRET_FILE, secretfile_headings, [8191, 131071])
 
 
